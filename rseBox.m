460f00adc6d0c2b29e114809595d8b5a8609db79
Thomas Otto <to7@st-andrews.ac.uk>
2018-04-16 18:09:38 +0100

    Update

---
+++ rseBox.m
@@ -0,0 +1,56 @@
+function rseBox
+%RSEBOX  Displays the help comments for all RSE-box functions. 
+%
+% Demonstration of RSE-box functions:
+%   demo01_quantiles  - Demonstration of the RSE-box: Basic RT analysis
+%   demo02_later      - Demonstration of the RSE-box: LATER model
+%   demo03_race       - Demonstration of the RSE-box: Race model
+%
+%
+% Simulation of reaction times:
+%   simLater          - Simulation of reaction times using the LATER model
+%   simRace           - Simulation of RSE data with a race model
+%   simRaceSimple     - Simulation of RSE data with the independent race model
+%
+%
+% Cumulative distribution functions (CDFs):
+%   outCorrect        - Outlier correction
+%   sampleDown        - Down-sampling of an empirical CDF
+%   getCP             - Computation of cumulative probabilities
+%   interpCDF         - Linear interpolation of an empirical CDF
+%   laterCDF          - Reci-normal CDF (LATER model)
+%   raceCDF           - Race model CDF
+%
+%
+% Basic RSE analysis:
+%   getRaab           - Computation of Raab's independent race model
+%   getGrice          - Computation of Grice's bound
+%   getGain           - Computation of redundancy gain (the RSE)
+%   getMiller         - Computation of Miller's bound
+%   getViolation      - Computation of Miller's bound violation
+%
+%
+% Model fitting:
+%   fitLater          - Fitting of the LATER model
+%   bootLater         - Bootstrapping 95% confidence intervals for LATER model fits
+%   fitRace           - MLE of the race model
+%   bootRace          - Bootstrapping 95% confidence intervals for race model fits
+%   fitRaceEta        - MLE of the race model (eta only)
+%   fitRaceRho        - MLE of the race model (rho only)
+%
+%
+% Model functions:
+%   maxNormPDF        - Computes the PDF of the maximum of two Gaussian random
+%   maxNormCDF        - Computes the CDF of the maximum of two Gaussian random
+%   minNormPDF        - Computes the PDF of the minimum of two Gaussian random
+%   minNormCDF        - Computes the CDF of the minimum of two Gaussian random
+%   t-function        - Owen's t-function (external code)
+%
+%
+% Copyright (C) 2017-18 Thomas Otto, University of St Andrews
+% http://lclab.wp.st-andrews.ac.uk/
+% See rseBox_license for details
+
+help rseBox
+
+end
